5d31bae1bbf71bded6de1bb64139fa37af6ad099
Mohanad Khador <mohanad@example.com>
2025-07-31 13:20:06 +0300

    تحديث إعدادات البناء والتثبيت

--- frontend/quiz-frontend/src/utils/audio.js
+++ frontend/quiz-frontend/src/utils/audio.js
@@ -1,12 +1,2 @@
-// src/utils/audio.js
-
-// أصوات التفاعلات
-// export const clickSound   = new Audio('/quiz/sounds/click.mp3')
-export const correctSound = new Audio('/quiz/sounds/correct.mp3')
-export const wrongSound   = new Audio('/quiz/sounds/wrong.mp3')
-
-// الموسيقى الخلفية
-// export const bgMusic      = new Audio('/quiz/sounds/bg-music.mp3')
-// bgMusic.loop = true
-
-// تمت إزالة tickSound و playTickLimited
+export const correctSound = new Audio('/static/sounds/correct.mp3');
+export const wrongSound   = new Audio('/static/sounds/wrong.mp3');
